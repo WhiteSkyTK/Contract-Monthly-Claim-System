0139cb5d9d2cf42f46214666d318296387d4bbb4
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-10-10 21:00:04 +0200

    added the module model

---
+++ Models/Module.cs
@@ -0,0 +1,9 @@
+﻿namespace Contract_Monthly_Claim_System.Models
+{
+    public class Module
+    {
+        public String ModuleCode { get; set; }
+        public String ModuleName { get; set; }
+        public String Description { get; set; }
+    }
+}
