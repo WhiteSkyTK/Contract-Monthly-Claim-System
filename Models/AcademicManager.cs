a7bc6a48360eac262578c0b1c3209e3ce87decaa
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-10-10 20:11:09 +0200

    Added the academic manager model

---
+++ Models/AcademicManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Contract_Monthly_Claim_System.Models
+{
+    public class AcademicManager
+    {
+        public int ManagerID { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerEmail { get; set; }
+    }
+}
