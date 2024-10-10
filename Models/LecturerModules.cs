a49bb2b7ae0251ea85da262551453867012c3ce8
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-10-10 21:07:09 +0200

    added LecturerModules model

---
+++ Models/LecturerModules.cs
@@ -0,0 +1,8 @@
+﻿namespace Contract_Monthly_Claim_System.Models
+{
+    public class LecturerModules
+    {
+        public int LecturerID { get; set; }
+        public string ModuleCode { get; set; }
+    }
+}
