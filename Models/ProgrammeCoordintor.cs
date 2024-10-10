8b2df5b87eebd422368b6cf51a9ae877a05eeb4c
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-10-10 20:13:34 +0200

    added the programme coordintor model

---
+++ Models/ProgrammeCoordintor.cs
@@ -0,0 +1,9 @@
+﻿namespace Contract_Monthly_Claim_System.Models
+{
+    public class ProgrammeCoordintor
+    {
+        public int CoordintorID { get; set; }
+        public string CoordintorName { get; set; }
+        public string CoordintorEmail { get; set; }
+    }
+}
