3a6eb9fc829c5e82ad9c1be7f970b1419c03c8f0
Tokollo NONYANE <ST10296818@rcconnect.edu.za>
2024-10-10 21:05:10 +0200

    ClaimsModules model added

---
+++ Models/ClaimsModules.cs
@@ -0,0 +1,8 @@
+﻿namespace Contract_Monthly_Claim_System.Models
+{
+    public class ClaimsModules
+    {
+        public int ClaimID { get; set; }
+        public string ModuleCode { get; set; }
+    }
+}
